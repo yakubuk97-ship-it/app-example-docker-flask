aef0acd6273c96c5efb68434a8144a09eb116623
yakubuk97-ship-it <yakubuk97@gmail.com>
2025-10-10 02:58:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def static_proxy(path):
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port)
+   port = int(os.getenv("PORT", "8080"))
+app.run(host="0.0.0.0", port=port)
