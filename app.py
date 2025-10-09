7fe21600c8723eb04935ee4499d39e8a4cb46bf2
yakubuk97-ship-it <yakubuk97@gmail.com>
2025-10-10 00:47:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder=".", static_folder=".")
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/<path:path>")
-def static_proxy(path):
-    return send_from_directory(".", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
