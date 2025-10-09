ca9a2c878661b699aadfacc07310565806e508d4
yakubuk97-ship-it <yakubuk97@gmail.com>
2025-10-10 00:43:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-import os
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".", static_folder=".")
 
 @app.route("/")
-def hello():
-    return "Timeweb Cloud + Docker + Flask = ❤️"
+def index():
+    return render_template("index.html")
+
+@app.route("/<path:path>")
+def static_proxy(path):
+    return send_from_directory(".", path)
 
 if __name__ == "__main__":
-    port = 3478
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(host="0.0.0.0", port=8080)
